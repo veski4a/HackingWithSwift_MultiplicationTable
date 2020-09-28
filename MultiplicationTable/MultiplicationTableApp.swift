0fa3bd6dd3124e52ffc92363a65c3d7ce32ca33a
Veselin Stefanov <veski4a92@gmail.com>
2020-09-28 07:56:01 +0300

    Initial Commit

---
+++ MultiplicationTable/MultiplicationTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTableApp.swift
+//  MultiplicationTable
+//
+//  Created by Veselin Stefanov on 28.09.20.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
